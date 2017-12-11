800bfce0843f8dca987c441c4c116a5b52ad01ac
jimmyzhou <15211133778@163.com>
2017-12-11 17:01:26 +0800

    first commit.

---
+++ js/draw_board.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var toolboxText = document.getElementById('toolbox').outerHTML;
+    var toolboxXml = Blockly.Xml.textToDom(toolboxText);
+
+    Blockly.inject('workspce_block', {
+        grid: {
+            spacing: 25,
+            length: 3,
+            colour: '#ccc',
+            snap: true
+        },
+        media:'media/',
+        toolbox: toolboxXml,
+        trashcan: true,
+        zoom: {
+            controls: true,
+            wheel: false
+        }
+    });
+
+});
